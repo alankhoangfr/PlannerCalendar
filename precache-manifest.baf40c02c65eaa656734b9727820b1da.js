a634133fc265a51ff9cc08754a9cf7fb8c4484f4
Alankhoang <alankhoangfr@gmail.com>
2019-05-13 22:58:09 +0200

    Updates

---
+++ precache-manifest.baf40c02c65eaa656734b9727820b1da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ab617cdafbe0609db5ddd19ad22282",
+    "url": "/PlannerCalendar/index.html"
+  },
+  {
+    "revision": "5c4a528e819e0ac5d4d6",
+    "url": "/PlannerCalendar/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/PlannerCalendar/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "5c4a528e819e0ac5d4d6",
+    "url": "/PlannerCalendar/static/js/main.f91661c4.chunk.js"
+  },
+  {
+    "revision": "e0e5551c170efcc81c98",
+    "url": "/PlannerCalendar/static/js/runtime~main.edd213c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PlannerCalendar/static/media/logo.5d5d9eef.svg"
+  }
+]);
